874d22057e443b74f3f2e5b81517b0709a892d26
Leonard Wong <me@leonardwong.net>
2021-03-14 22:32:36 +0800

    Initial commit

---
+++ Default.aspx
@@ -0,0 +1,57 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="SMUSOP.Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <!-- CSS only -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous" />
+    <!-- JavaScript Bundle with Popper -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.bundle.min.js" integrity="sha384-b5kHyXgcpbZJO/tY9Ul7kGkf1S0CWuKcCD38l8YkeH8z8QjE0GmW1gYU5S9FOnJ0" crossorigin="anonymous"></script>
+    <title>SMUSOP</title>
+
+</head>
+<body>
+    <div class="row">
+        <div class="container">
+            <h2 style="text-align: center">SMU SOP</h2>
+            <br />
+            <div class="row align-items-start">
+                <div class="col-s12">
+                    <div class="col-12">
+                        <div class="row">
+                            <div class="col-2 offset-2">
+                                <div style="background-color: lightgreen; width: 150px; padding: 10px; margin-right: 30px; color: black; font-size: 13px; font-weight: bold; text-align: center;">Available</div>
+                            </div>
+                            <div class="col-2">
+                                <div style="background-color: orange; width: 150px; padding: 10px; margin-right: 30px; color: black; font-size: 13px; font-weight: bold; text-align: center">Occupied by Object</div>
+                            </div>
+                            <div class="col-4">
+                                <div style="background-color: red; width: 150px; padding: 10px; color: white; font-size: 13px; font-weight: bold; text-align: center;">Occupied by Human</div>
+                            </div>
+                        </div>
+                    </div>
+                    <br />
+                    <div class="row" style="margin-top:50px;">
+                        <div class="col-8 offset-4">
+
+
+                            <asp:Label ID="pin1" runat="server" Style="width: 150px; height: 150px;padding:50px;">Seat 1</asp:Label>
+                            
+                            <asp:Label ID="pin2" runat="server" Style="width: 150px; height: 150px;padding:50px;">Seat 2</asp:Label>
+                            
+                            <asp:Label ID="pin3" runat="server" Style="width: 150px; height: 150px;padding:50px;">Seat 3</asp:Label>
+
+                        </div>
+
+                    </div>
+
+                </div>
+            </div>
+        </div>
+
+
+    </div>
+
+</body>
+</html>
---
+++ Default.aspx.cs
@@ -0,0 +1,110 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Drawing;
+
+namespace SMUSOP
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                RetrieveData();
+
+            }
+
+        }
+
+
+        public void RetrieveData()
+        {
+            WebClient webClient = new WebClient();
+            string myJSON = webClient.DownloadString("https://dweet.io/get/dweets/for/SMUSOP");
+
+            dynamic jsonObj = JsonConvert.DeserializeObject(myJSON);
+            for (int i = 0; i < jsonObj["with"].Count; i++)
+            {
+                //1st pin
+                string object1 = jsonObj["with"][0]["content"]["object1"].ToString();
+                string objectThreshold1 = jsonObj["with"][0]["content"]["objectThreshold1"].ToString();
+
+                string people1 = jsonObj["with"][0]["content"]["people1"].ToString();
+                string peopleThreshold1 = jsonObj["with"][0]["content"]["peopleThreshold1"].ToString();
+
+                if (Convert.ToDouble(people1) > Convert.ToDouble(peopleThreshold1)) {
+                    //People occupied
+                    pin1.BackColor = Color.Red;
+                    pin1.ForeColor = Color.White;
+                }
+                else if (Convert.ToDouble(object1) > Convert.ToDouble(objectThreshold1))
+                {
+                    //Object Occupied
+                    pin1.BackColor = Color.Orange;
+                }
+                else
+                {
+                    //Available
+                    pin1.BackColor = Color.LightGreen;
+                }
+
+                //2nd pin
+                string object2 = jsonObj["with"][0]["content"]["object2"].ToString();
+                string objectThreshold2 = jsonObj["with"][0]["content"]["objectThreshold2"].ToString();
+
+                string people2 = jsonObj["with"][0]["content"]["people2"].ToString();
+                string peopleThreshold2 = jsonObj["with"][0]["content"]["peopleThreshold2"].ToString();
+
+                if (Convert.ToDouble(people2) > Convert.ToDouble(peopleThreshold2))
+                {
+                    //People occupied
+                    pin2.BackColor = System.Drawing.Color.Red;
+                }
+                else if (Convert.ToDouble(object2) > Convert.ToDouble(objectThreshold2))
+                {
+                    //Object Occupied
+                    pin2.BackColor = System.Drawing.Color.Orange;
+                }
+                else
+                {
+                    //Available
+                    pin2.BackColor = System.Drawing.Color.LightGreen;
+                }
+
+                //3rd pin
+                string object3 = jsonObj["with"][0]["content"]["object3"].ToString();
+                string objectThreshold3 = jsonObj["with"][0]["content"]["objectThreshold3"].ToString();
+
+                string people3 = jsonObj["with"][0]["content"]["people3"].ToString();
+                string peopleThreshold3 = jsonObj["with"][0]["content"]["peopleThreshold3"].ToString();
+
+                if (Convert.ToDouble(people3) > Convert.ToDouble(peopleThreshold3))
+                {
+                    //People occupied
+                    pin3.BackColor = System.Drawing.Color.Red;
+                }
+                else if (Convert.ToDouble(object3) > Convert.ToDouble(objectThreshold3))
+                {
+                    //Object Occupied
+                    pin3.BackColor = System.Drawing.Color.Orange;
+                }
+                else
+                {
+                    //Available
+                    pin3.BackColor = System.Drawing.Color.LightGreen;
+                }
+
+
+            }
+        }
+
+    }
+
+
+}
